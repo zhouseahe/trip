fad7755084afd9d5e035ab4c088cf07296909d55
zhoushaohe <Monster&912>
2016-03-21 10:39:22 +0800

    basic

---
+++ common/WebConstant.js
@@ -0,0 +1,7 @@
+/**
+ * Created by zhoushaohe on 2016/3/21.
+ */
+module.exports = {
+    title : "农家乐",
+    description : "近郊旅游直通车 。。。"
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var site = require('../common/WebConstant.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: site.title });
 });
 
 module.exports = router;
