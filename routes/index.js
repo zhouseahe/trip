8480025134901f794f2cd0951655832cac5e64da
unknown <zhouseahe@gmail.com>
2016-05-01 22:11:22 +0800

    recommend

--- routes/index.js
+++ routes/index.js
@@ -59,10 +59,27 @@ router.get('/search',function(req, res, next){
                 res.render('error', {message:'查询出错了！'});
                 return ;
             }
+            if(results[0].length==0){
+
+            }
             var pages = pageUtil.pageList(page,results[1]);
             var url = "/search?keyWord=" + keyWord +"&page=";
             res.render('items', {title:site.title,items:results[0],keyWord:keyWord,page:page,pages:pages,url:url,count:Math.ceil(results[1]/site.page_count_limit)});
         });
+});
+
+router.get('/recommend',function(req, res, next){
+    var keyWord = '';
+    var page = Math.floor(Math.random()*10+1);
+    var limit = site.page_count_limit;
+    var options = { skip: (page - 1) * limit, limit: limit};
+    var param = {query:{search:new RegExp(keyWord)},page:page,options:options};
+    topicService.find(param,function(err,topics){
+        if(err){
+            return ;
+        }
+        res.render('recommend', { items:topics});
+    });
 });
 
 module.exports = router;
